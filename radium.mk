49a08b3d4048b9bb39625ccb3fded86c9e607934
notiflux <schrott_07@t-online.de>
2015-08-19 16:07:27 +0200

    Update and rename cm.mk to radium.mk

--- radium.mk
+++ radium.mk
@@ -1,9 +1,9 @@
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common RADIUM stuff
+$(call inherit-product, vendor/radium/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/armani/full_armani.mk)
 
-PRODUCT_NAME := cm_armani
+PRODUCT_NAME := radium_armani
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
